f143964ff8aa36d3f17e72737258067ee3670ce4
piotrdudzinski2-web <piotrdudzinski2@gmail.com>
2025-07-25 22:28:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return 'AI Niemiecki Asystent działa!'
+from flask import render_template
+
+@app.route("/audio")
+def audio():
+    return render_template("audio.html")
